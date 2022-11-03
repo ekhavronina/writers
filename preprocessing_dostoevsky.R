c8d3893c2b78ea67c7475d57d3a6cd380ad13ac7
ekhavronina <90327225+ekhavronina@users.noreply.github.com>
2022-11-03 21:35:45 +0300

    added

--- preprocessing_dostoevsky.R
+++ preprocessing_dostoevsky.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(readr)
 library(stringr)
 library(dplyr)
@@ -84,96 +83,6 @@ dos_dtm <- dostoevsky_words %>%
 dos_dtm
 
 dos_clean <- dos_dtm %>%
-  dfm_wordstem(language = "ru") 
-# %>% dfm_trim(min_docfreq=0.10) 
+  dfm_wordstem(language = "ru") %>% 
+  dfm_trim(min_docfreq=0.10)     # ?
 dos_clean
-=======
-library(readr)
-library(stringr)
-library(dplyr)
-library(tidytext)
-library(stopwords)
-library(quanteda)
-
-setwd('C:/Users/79175/Documents/rwd')
-
-
-dostoevsky_all <- read_csv('writers_project/dostoevsky_all.csv', 
-                           locale = readr::locale(encoding = "cp1251"))
-
-
-dostoevsky_all$clean <- gsub("[^[:alnum:]\\<>чЧёЁ\\(\\)]", " ", dostoevsky_all$text)
-dostoevsky_all$clean <- str_replace_all(dostoevsky_all$clean, ">", "")
-dostoevsky_all$clean <- str_replace_all(dostoevsky_all$clean, "<", "")
-dostoevsky_all$clean <- str_replace_all(dostoevsky_all$clean, "\\)", "")
-dostoevsky_all$clean <- str_replace_all(dostoevsky_all$clean, "\\(", "")
-dostoevsky_all$clean <- str_replace_all(dostoevsky_all$clean, "[0-9]+", " ")
-dostoevsky_all$clean <- str_replace_all(dostoevsky_all$clean, "[\n\t\r]+", " ")
-dostoevsky_all$clean <- str_squish(dostoevsky_all$clean)
-
-
-View(head(dostoevsky_all))
-dostoevsky_all$clean[1]
-dostoevsky_all$text[1]
-
-dostoevsky_clean <- dostoevsky_all %>% 
-  select(title, clean)
-
-# saving
-#write.csv(dostoevsky_clean,"dostoevsky_clean.csv", row.names = FALSE)
-
-
-
-# importing cleaned data:
-#dostoevsky_clean <- read_csv('writers_project/dostoevsky_clean.csv', 
-#                             locale = readr::locale(encoding = "cp1251"))
-
-
-
-### preprocessing
-
-dostoevsky_gram <- dostoevsky_clean %>% 
-  group_by(title) %>% 
-  unnest_tokens(gram, clean)
-
-dostoevsky_gram$word <- system2("mystem", c("-d", "-l", "-c"),
-                                  input = dostoevsky_gram$gram, stdout = TRUE)
-
-View(head(dostoevsky_gram))
-
-# saving
-#write.csv(dostoevsky_gram,"dostoevsky_gram.csv", row.names = FALSE)
-
-dostoevsky_words <- read_csv('writers_project/dostoevsky_gram.csv')
-
-
-dostoevsky_words <- dostoevsky_gram
-dostoevsky_words$word <-gsub("[[:punct:]]", "", dostoevsky_words$word)
-
-sw <- stopwords('ru')
-dostoevsky_words <- dostoevsky_words %>% 
-  select(title, word) %>% 
-  filter(!word %in% sw)
-
-
-View(head(dostoevsky_words))
-
-# this list contains Roman alphabet
-dos_freq <- dostoevsky_words %>% group_by(word) %>% count() %>% arrange(desc(n))
-
-
-### calculating TF-IDF DFM (document-feature matrix)
-
-dostoevsky_words <- dostoevsky_words %>% 
-  mutate(id = row_number())
-dos_dtm <- dostoevsky_words %>%
-  count(id, word) %>%
-  bind_tf_idf(word, id, n) %>% 
-  cast_dfm(id, word, tf_idf)
-dos_dtm
-
-dos_clean <- dos_dtm %>%
-  dfm_wordstem(language = "ru") 
-# %>% dfm_trim(min_docfreq=0.10) 
-dos_clean
->>>>>>> 8fac51d50d9d436cd1edccac26c9bb8b2a34b7a3
